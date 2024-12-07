27ed3e8a73e15ab5144f06987443cc710045edcc
rodrigorvsn <rodrigovictor81@gmail.com>
2024-12-07 17:13:10 -0300

    feat: add part 2 of day 1

--- 1/main.go
+++ 1/main.go
@@ -29,5 +29,17 @@ func main() {
 	for index := 0; index < len(splittedByLine)-1; index++ {
 		count += math.Abs(float64(right[index]) - float64(left[index]))
 	}
-	fmt.Printf("%f", count)
+	fmt.Printf("Difference total count: %f.0", count)
+
+	foundTotalCount := 0
+	for index := 0; index < len(left)-1; index++ {
+		foundCount := 0
+		for rightIndex := 0; rightIndex < len(right)-1; rightIndex++ {
+			if right[rightIndex] == left[index] {
+				foundCount += 1
+			}
+		}
+		foundTotalCount += left[index] * foundCount
+	}
+	fmt.Printf("\nFound total count: %d", foundTotalCount)
 }
