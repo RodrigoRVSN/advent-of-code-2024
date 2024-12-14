d746ad973b730925563d2135b7d303bc6c1cd4ee
rodrigorvsn <rodrigovictor81@gmail.com>
2024-12-14 10:09:07 -0300

    feat: add day 5 part 2

--- 5/main.go
+++ 5/main.go
@@ -17,6 +17,25 @@ func indexOf(element string, data []string) int {
 	return -1
 }
 
+func sortSequence(values []string, rules []string) []string {
+	for {
+		swapped := false
+		for _, rule := range rules {
+			ruleSide := strings.Split(rule, "|")
+			left := indexOf(ruleSide[0], values)
+			right := indexOf(ruleSide[1], values)
+			if left > right && right != -1 && left != -1 {
+				values[left], values[right] = values[right], values[left]
+				swapped = true
+			}
+		}
+		if !swapped {
+			break
+		}
+	}
+	return values
+}
+
 func main() {
 	file, _ := os.Open("./input.txt")
 	fileConverted, _ := io.ReadAll(file)
@@ -26,6 +45,8 @@ func main() {
 	rules := strings.Split(sections[0], "\n")
 	sequences := strings.Split(sections[1], "\n")
 	totalCount := 0
+	totalOrderedCount := 0
+
 	for _, sequence := range sequences {
 		isValidSequence := true
 		previousSequences := []string{}
@@ -38,16 +59,25 @@ func main() {
 					index := indexOf(ruleSide[0], values)
 					if index > valueIndex {
 						isValidSequence = false
-						break
 					}
 				}
 			}
 		}
+
+		if !isValidSequence {
+			sortedValues := sortSequence(values, rules)
+			middleIndex := len(sortedValues) / 2
+			valueAsNumber, _ := strconv.Atoi(string(sortedValues[middleIndex]))
+			totalOrderedCount += valueAsNumber
+		}
+
 		if isValidSequence {
 			middleIndex := len(values) / 2
 			valueAsNumber, _ := strconv.Atoi(string(values[middleIndex]))
 			totalCount += valueAsNumber
 		}
 	}
-	fmt.Println(totalCount)
+
+	fmt.Println("Total: ", totalCount)
+	fmt.Println("Total after ordering it: ", totalOrderedCount)
 }
